6ffe6747db7c871b2c2ae6df45205472e0eddcf8
Ning Ning Sun <nsun200@live.com>
2016-09-17 19:26:46 -0400

    Rename app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,12 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
 
 if __name__== "__main__":
     app.run()
