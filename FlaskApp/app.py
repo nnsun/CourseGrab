407f57f1aa239f4b2616a577c386c2c5c2e8cade
Ning Ning Sun <nsun200@live.com>
2016-09-17 19:16:45 -0400

    mySQL set up

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
 if __name__== "__main__":
     app.run()
