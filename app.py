dd44b5af2894a70f95c321add65124599e16fe65
Ning Ning Sun <nsun200@live.com>
2016-09-17 19:21:14 -0400

    Bring Flask files to root

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+if __name__== "__main__":
+    app.run()
