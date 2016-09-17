b582bf3884f3dee74a115c99aec7aacd6ab0ce3a
Ning Ning Sun <nsun200@live.com>
2016-09-17 19:46:32 -0400

    Current app

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,9 +11,13 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
-    _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+
+    if  _email and _password:
+        return json.dumps({'html':'<span>All fields good</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 if __name__== "__main__":
     app.run()
