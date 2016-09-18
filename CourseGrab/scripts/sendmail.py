0e93297ed8d28e5c581b3bc772defa250c61fcbd
Ning Ning Sun <nsun200@live.com>
2016-09-18 03:03:24 -0400

    Email working[B

--- CourseGrab/scripts/sendmail.py
+++ CourseGrab/scripts/sendmail.py
@@ -5,5 +5,5 @@ def send_email(email_address, course_code):
     smtpObj.ehlo()
     smtpObj.starttls()
     smtpObj.login("cornellcoursegrab@gmail.com", "coursegrab")
-    smtpObj.sendmail("cornellcoursegrab@gmail.com", "nsun200@live.com", "Subject: Course number %s is now open!\n" % course_code)
+    smtpObj.sendmail("cornellcoursegrab@gmail.com", email_address, "Subject: Course number %s is now open!\n" % course_code)
     smtpObj.quit()
