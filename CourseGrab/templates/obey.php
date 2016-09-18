bd1b2de4b8e1620eb80407930851c5c5b6d820d2
Chase Thomas <chase@chasethomas.com>
2016-09-18 04:32:30 -0400

    flask running again

--- CourseGrab/templates/obey.php
+++ CourseGrab/templates/obey.php
@@ -1,21 +1,19 @@
-<html>
-  <head>
-    <title>Thanks! <br> We will send an email to ["email"] when the course is open.</title>
-  </head>
-  <?php
-    header( 'Location: success.html' )
+<?php
     $email = $_POST['email'];
     $number = $_POST['course_number'];
     if(empty($email) || empty($number))
-      {//show the form
-      $message = 'Fill in areas in red!';
-      $aClass = 'errorClass';
+        {//show the form
+        $message = 'Fill in areas in red!';
+        $aClass = 'errorClass';
     }
     $cvsData = $email . "," . $number . "\n";
     $fp = fopen("../scripts/obey.csv","a");
     if($fp){
-      fwrite($fp,$cvsData); // Write information to the file
-      fclose($fp); // Close the file
+     fwrite($fp,$cvsData); // Write information to the file
+     fclose($fp); // Close the file
     }
-  ?>
-</html>
+    if(processing == success) {
+      header("Location:success.html");
+      exit();
+    }
+?>
