4aeba16a9bc13c345694dfbda34bcf214be6e308
Ning Ning Sun <nsun200@live.com>
2016-09-18 08:34:32 -0400

    EVERYTHING WORKSgit add -A! :)

--- obj/Release/Package/PackageTmp/CourseGrab/__init__.py
+++ obj/Release/Package/PackageTmp/CourseGrab/__init__.py
@@ -3,6 +3,6 @@ The flask application package.
 """
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 
 import CourseGrab.views
---
+++ scripts/run.py
@@ -0,0 +1,19 @@
+import check_course
+import sys
+sys.path.append("site-packages")
+import sendmail
+import csv
+import read_dict
+
+
+with open("ledger.csv", 'rU') as codes:
+    code_reader = csv.reader(codes)
+    while True:
+        try:
+            code_line = code_reader.next()
+            email = code_line[0]
+            code = code_line[1]
+            if check_course.check_course(code):
+                sendmail.send_email(email, code)
+        except StopIteration:
+            break
