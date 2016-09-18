9011c8320e55924896c342fd3b60f055123b62e4
Ning Ning Sun <nsun200@live.com>
2016-09-17 22:33:36 -0400

    Fix home button

--- CourseGrab/views.py
+++ CourseGrab/views.py
@@ -16,22 +16,36 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    if  _email and _password:
+        return json.dumps({'html':'<span>All fields good</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+#@app.route('/contact')
+#def contact():
+#    """Renders the contact page."""
+#    return render_template(
+#        'contact.html',
+#        title='Contact',
+#        year=datetime.now().year,
+#        message='Your contact page.'
+#    )
+
+#@app.route('/about')
+#def about():
+#    """Renders the about page."""
+#    return render_template(
+#        'about.html',
+#        title='About',
+#        year=datetime.now().year,
+#        message='Your application description page.'
+#    )
