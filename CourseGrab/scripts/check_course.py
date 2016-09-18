127e728414d6320346ff80d71b91f2577719fc39
Ning Ning Sun <nsun200@live.com>
2016-09-18 06:12:10 -0400

    Before pull

--- CourseGrab/scripts/check_course.py
+++ CourseGrab/scripts/check_course.py
@@ -2,6 +2,8 @@ import sys
 from read_dict import course_map
 import requests
 import bs4
+import csv
+import smtplib
 
 
 def check_course(code):
@@ -18,3 +20,29 @@ def check_course(code):
                 return True
             return False
 
+
+
+def read_dict():
+    course_map = {}
+    with open("course_subject_dict.csv", 'rU') as dict_file:
+        dict_reader = csv.reader(dict_file)
+        while True:
+            try:
+                dict_line = dict_reader.next()
+                course_map[int(dict_line[0])] = dict_line[1]
+            except StopIteration:
+                break
+    return course_map
+
+def send_email(email_address, course_code):
+    smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login("cornellcoursegrab@gmail.com", "coursegrab")
+    smtpObj.sendmail("cornellcoursegrab@gmail.com", email_address, "Subject: Course number %s is now open!\n" % course_code)
+    smtpObj.quit()
+
+
+
+if check_course(code):
+    sendmail.send_email(email_address, code)
