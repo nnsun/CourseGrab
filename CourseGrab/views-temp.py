8a2ed601911e2d8c517d981b1634d355b6bbaf4c
Chase Thomas <chase@chasethomas.com>
2016-09-18 01:31:51 -0400

    migrated from Flask

--- CourseGrab/views-temp.py
+++ CourseGrab/views-temp.py
@@ -5,19 +5,5 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
-
-@app.route('/signUp',methods=['POST'])
-def signUp():
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-
-    if  _email and _password:
-        return json.dumps({'html':'<span>All fields good</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-
 if __name__== "__main__":
     app.run()
