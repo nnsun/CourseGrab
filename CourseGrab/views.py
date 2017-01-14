240dd9d406cd777b647fc03b8f6d5b9ae16d0b59
Ning Ning Sun <nsun200@live.com>
2017-01-14 12:33:36 -0500

    Ensure website always uses SSL

--- CourseGrab/views.py
+++ CourseGrab/views.py
@@ -3,57 +3,75 @@ Routes and views for the Flask application.
 """
 
 from datetime import datetime
-from flask import render_template, send_from_directory, request
+from flask import render_template, request, session, redirect, url_for
 from CourseGrab import app
 from CourseGrab.models.sql_client import Client
+from CourseGrab import google
 
 
-"""
-Display the home page
-"""
 @app.route('/')
 @app.route('/home')
-def home():
+def index():
+    #access_token = session.get('access_token')
+    #if access_token is None:
+    #    return redirect(url_for('sign_in'))
+ 
+    #access_token = access_token[0]
+    #from urllib2 import Request, urlopen, URLError
+ 
+    #headers = {'Authorization': 'OAuth '+access_token}
+    #req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+    #              None, headers)
+    #try:
+    #    res = urlopen(req)
+    #except URLError, e:
+    #    if e.code == 401:
+    #        # Unauthorized - bad token
+    #        session.pop('access_token', None)
+    #        return redirect(url_for('sign_in'))
+    #    return res.read()
+ 
+    #return res.read()
     return render_template('index.html')
-
-
-"""
-
-"""
-@app.route('/submitted', methods=['POST'])
-def submit_request():
-    if request.method == 'POST':
-        course_code = request.form["course_number"]
-        client = Client()
-        client.submit_request("nsun200@live.com", course_code)
-        client.connection.close()
-        return render_template("success.html")
-
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
 
 @app.route('/sign_in')
 def sign_in():
-    return render_template('sign_in.html')
+    callback = url_for('authorized', _external = True)
+    return google.authorize(callback=callback)
+ 
+ 
+ 
+@app.route('/oauth2callback')
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+ 
+ 
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+
+
+
+#@app.route('/submitted', methods=['POST'])
+#def submit_request():
+#    if request.method == 'POST':
+#        course_code = request.form["course_number"]
+#        client = Client()
+#        client.submit_request("nsun200@live.com", course_code)
+#        client.connection.close()
+#        return render_template("success.html")
+
+
+#@app.route('/sign_in')
+#def sign_in():
+#    return render_template('sign_in.html')
+
+
+
 
 
 @app.errorhandler(400)
--- runserver.py
+++ runserver.py
@@ -8,8 +8,10 @@ from CourseGrab import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
+        # Order of PORT assignments in try/except switched in order
+        # to keep port constant when debugging
         PORT = 5555
+    except ValueError:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     app.run(HOST, PORT)
     
