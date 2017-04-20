d73d784c59903b18aa36f936bd028b5ba25b4c9f
Ning Ning Sun <nsun200@live.com>
2017-04-20 15:26:14 -0400

    Restructure project: Notifier and web app now in separate projects

---
+++ CourseGrab/views.py
@@ -0,0 +1,108 @@
+"""
+Routes and views for the Flask application.
+"""
+
+from flask import render_template, request, session, redirect, url_for, flash
+from urllib2 import Request, urlopen, URLError
+import json
+from CourseGrab import app
+from CourseGrab.models.sql_client import Client
+from CourseGrab import google
+
+
+@app.route('/')
+def index():
+    courses = None
+    access_token = session.get('access_token')
+    course_list = []
+    if access_token is not None:
+        user_dict = get_user_dict()
+        user_id = user_dict["id"]
+        client = Client()
+        course_list = client.get_courses(user_id)
+    return render_template('index.html', course_list = course_list)
+
+
+@app.route('/sign_in')
+def sign_in():
+    callback = url_for('authorized', _external = True)
+    return google.authorize(callback=callback)
+ 
+ 
+@app.route('/oauth2callback')
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    user_dict = get_user_dict()
+    user_id = user_dict["id"]
+    user_email = user_dict["email"]
+    client = Client()
+    client.add_user(user_id, user_email)
+    return redirect(url_for('index'))
+ 
+ 
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+
+@app.route('/submitted', methods = ['POST'])
+def submit_request():
+    if session.get('access_token') is None:
+        flash("Please sign in first.")
+    else:
+        user_dict = get_user_dict()
+        user_id = user_dict["id"]
+        course_code = request.form["course_number"]
+        client = Client()
+        try:
+            client.submit_request(user_id, course_code)
+            client.connection.close()
+        except UserWarning as err:
+            flash(str(err))
+    return redirect(url_for('index'))
+
+
+@app.route('/remove/<int:course_num>', methods = ['POST'])
+def remove(course_num):
+    user_dict = get_user_dict()
+    user_id = user_dict["id"]
+    client = Client()
+    client.remove_course(user_id, course_num)
+    return redirect(url_for('index'))
+
+
+@app.route('/sign_out')
+def sign_out():
+    # remove the token from the session if it's there
+    session.pop('access_token', None)
+    return redirect(url_for('index'))
+
+
+def get_user_dict():
+    access_token = session.get('access_token')
+    if (access_token is None):
+        return redirect("google.com")
+    access_token = access_token[0]
+    headers = {'Authorization': 'OAuth ' + access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    res = urlopen(req)
+    
+    return json.loads(res.read())
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return "400 error", 400
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "404 error", 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return "500 error. We will fix this someday but in the meantime, just <a href='https://cornellcoursegrab.com'>click here</a> to return to the main page.", 500
---
+++ course_parser.py
@@ -0,0 +1,43 @@
+import requests
+import bs4
+from CourseGrab.models.sql_client import Client
+
+
+"""
+Parses the Cornell class roster and builds a SQL database of all courses
+"""
+def main():
+    client = Client()
+    course_num_map = {}
+    roster_page = "https://classes.cornell.edu/browse/roster/SP17"
+    roster_request = requests.get(roster_page)
+    roster_request.raise_for_status()
+    roster_bs4 = bs4.BeautifulSoup(roster_request.text, "html.parser")
+    subject_tags = roster_bs4.select(".browse-subjectcode")
+
+    subject_list = []
+    for tag in subject_tags:
+        subject_list.append(str(tag.getText()))
+
+    subjects_page = "https://classes.cornell.edu/browse/roster/FA17/subject/"
+    # Section information is displayed as "Class Section ABC 123".
+    # The offset enables grabbing only the relavent section information.
+    section_offset = len("Class Section ")      
+
+    for subject_code in subject_list:
+        print subject_code
+        subject_request = requests.get(subjects_page + subject_code)
+        subject_bs4 = bs4.BeautifulSoup(subject_request.text, "html.parser")
+        course_code_tags = subject_bs4.find_all("strong", class_ = "tooltip-iws")
+        for tag in course_code_tags:
+            course_num = int(tag.getText().strip())
+            catalog_num = int("".join([x for x in tag.next_sibling.getText() if x.isdigit()]))
+            title = tag.parent.parent.parent.parent.parent.parent.find_all("div", class_ = "title-coursedescr")[0].getText()
+            section = str(tag.parent.parent.parent["aria-label"])[section_offset:]
+            command = "INSERT INTO Courses (CourseNum, OpenStatus, SubjectCode, CatalogNum, Title, Section) VALUES (?, 0, ?, ?, ?, ?)"
+            client.cursor.execute(command, [course_num, subject_code, catalog_num, title, section])
+    client.connection.commit()
+    client.connection.close()
+
+if __name__ == "__main__":
+    main()
