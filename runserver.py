4f3c7f325710e6afa74966c4e16862943c4d1a75
Ning Ning Sun <nsun200@live.com>
2017-04-20 16:23:44 -0400

    Update runserver for WSGI entry

--- runserver.py
+++ runserver.py
@@ -3,15 +3,7 @@ This script runs the CourseGrab application using a development server.
 """
 
 from os import environ
-from CourseGrab import app
+from CourseGrab import app as application
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        # Order of PORT assignments in try/except switched in order
-        # to keep port constant when debugging
-        PORT = 5555
-    except ValueError:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    app.run(HOST, PORT)
-    
+    app.run()
